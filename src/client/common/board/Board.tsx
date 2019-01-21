332efa765ad276bdaaaf7facc06542be8499838c
Ty Smith <dts5004@gmail.com>
2019-01-20 21:09:01 -0800

    Started adding code to generate the board

--- src/client/common/board/Board.tsx
+++ src/client/common/board/Board.tsx
@@ -1,5 +1,7 @@
 import { Color } from "csstype";
 import * as React from "react";
+import "./board.styles";
+import { Pawn } from "./Pawn";
 
 export interface IProps {
     darkcolor: Color;
@@ -8,16 +10,22 @@ export interface IProps {
 
 export const Board = (props: IProps) => {
     const rects = [];
+    const pawns = [];
     for (let rank = 0; rank < 8; rank++) {
         for (let file = 0; file < 8; file++) {
             const color = (rank + file) % 2 === 0 ? props.darkcolor : props.lightColor;
-            const rect = <rect width={64} height={64} x={file * 64} y={512 - (64 * (rank + 1))} fill={color}/>;
+            const rect = <rect width={64} height={64} x={file * 64} y={512 - (64 * (rank + 1))} fill={color} />;
             rects.push(rect);
         }
     }
     return (
-        <svg viewBox="0 0 512 512">
-            {rects}
-        </svg>
+        <div className="container">
+            <svg className="board" viewBox="0 0 512 512">
+                {rects}
+            </svg>
+            <svg className="pieces" viewBox="0 0 512 512">
+                <Pawn strokeColor="blue" fillColor="green" />
+            </svg>
+        </div>
     );
 };
