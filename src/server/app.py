1d02bbab15ef67cb27f5c91d88d146eae5c8de44
Ty Smith <dts5004@gmail.com>
2019-01-24 21:52:38 -0800

    Attempting to use connexion

--- src/server/app.py
+++ src/server/app.py
@@ -1,12 +1,21 @@
 import connexion
 
-app = connexion.App(__name__, specification_dir="./")
-
-app.add_api("swagger.yml")
-
-@app.route("/")
 def home():
     return "hello world"
+
+def moves():
+    fen = connexion.request.get("fen")
+    return fen
+
+def openings():
+    return "A00"
+
+def openings_single():
+    id = connexion.request.get("id")
+    return id
+
+app = connexion.FlaskApp(__name__)
+app.add_api("swagger.yaml")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
