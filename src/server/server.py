e7513d6787f3c02dbc9f15798e201363c0454681
Ty Smith <dts5004@gmail.com>
2019-02-15 22:35:30 -0800

    Got flask serving stastic and api

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,30 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
+
+from_main = False
 
 app = Flask(__name__)
 
-from app import routes
+print("global")
+
+@app.route("/api")
+def api():
+    return "/API!!"
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    global from_main
+    base = "../../dist/client"
+    if path == "" or path == "/":
+        path = "index.html"
+    if from_main:
+        base = "client"
+    base = os.path.abspath(base)
+    if os.path.exists(os.path.join(base, path)):
+        return send_from_directory(base, path)
+    return "404"
+
+if __name__ == "__main__":
+    from_main = True
+    app.run(port=8080)
