aea520e470dea83118fbaef94f1d13b1cd698bea
Ty Smith <dts5004@gmail.com>
2019-02-15 20:23:51 -0800

    Removed all swagger items

--- src/server/chess-api/swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.fen_request import FenRequest
-from swagger_server.models.move import Move
-from swagger_server.models.opening import Opening
-from swagger_server.models.opening_variant import OpeningVariant
