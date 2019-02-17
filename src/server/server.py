ac171da90eb05360c60ed3995af83db988957da8
Ty Smith <dts5004@gmail.com>
2019-02-16 16:47:42 -0800

    Readded openings with guid

--- src/server/server.py
+++ src/server/server.py
@@ -1,15 +1,33 @@
+import chess
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
+from openings import OPENINGS
 
 from_main = False
 
 app = Flask(__name__)
 
-print("global")
+@app.route("/api/openings", methods=["GET"])
+def openings():
+    return jsonify(OPENINGS)
 
-@app.route("/api")
-def api():
-    return "/API!!"
+@app.route("/api/moves", methods=["POST"])
+def moves():
+    game_moves = list(request.get_json()["moves"])
+    board = chess.Board()
+    ret = []
+    for move in game_moves:
+        src = move["src"]
+        dst = move["dst"]
+        board.push_uci("{}{}".format(src, dst))
+    for move in board.generate_legal_moves():
+        ret.append({
+            "src": chess.square_name(move.from_square),
+            "dst": chess.square_name(move.to_square)
+        })
+    return jsonify(ret)
+
+    
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
