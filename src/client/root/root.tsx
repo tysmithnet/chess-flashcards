97bc8ea9c2e3763fff0092822e3e14d5d29b4542
Ty Smith <dts5004@gmail.com>
2019-03-11 14:32:02 -0700

    Removing old comments

--- src/client/root/root.tsx
+++ src/client/root/root.tsx
@@ -1,5 +1,5 @@
 import { createMuiTheme, CssBaseline, MuiThemeProvider } from "@material-ui/core";
-import { install, ThemeProvider } from "@material-ui/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
@@ -7,8 +7,6 @@ import { Provider } from "react-redux";
 import App from "../app/App";
 import { rootSaga } from "./root.saga";
 import { configureStore, sagaMiddleware } from "./root.store";
-
-// install();
 
 const theme = createMuiTheme({ typography: { useNextVariants: true } });
 const store = configureStore({
@@ -22,11 +20,6 @@ const store = configureStore({
     router: null,
 });
 
-/**
- * Render a component and connect the root store to it
- *
- * @param {JSX.Element} component
- */
 function render(component: JSX.Element) {
     ReactDOM.render(
         <CssBaseline>
@@ -42,8 +35,5 @@ function render(component: JSX.Element) {
     );
 }
 
-// Start the "background processing"
 sagaMiddleware.run(rootSaga);
-
-// Render the application
 render(<App />);
